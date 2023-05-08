e6a8fe74d99cec8546a8d70997928c31d9bad482
Bartroz <rozwadowski512@gmail.com>
2023-05-08 21:05:11 +0200

    Changed directory of index.html to could be hosted on github pages

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 	devtool: 'source-map',
 	devServer: {
 		static: {
-			directory: path.join(__dirname, 'src'),
+			directory: path.join(__dirname, 'docs'),
 		},
 		port: 9000,
 	},
