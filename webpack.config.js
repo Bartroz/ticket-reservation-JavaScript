76f88e9bee3480409c245e989d2d4d9c2f4f633f
Bartroz <rozwadowski512@gmail.com>
2023-05-21 18:02:34 +0200

    Changed position of login-panel close button

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
 	devtool: 'source-map',
 	devServer: {
 		static: {
-			directory: path.join(__dirname, 'dist'),
+			directory: path.join(__dirname, 'src'),
 		},
 		port: 9000,
 	},
